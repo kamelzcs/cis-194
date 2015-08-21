630c8d51f8c6bd23bb5e333e9b27f515359212e6
kamelzcs <kamelzcs@gmail.com>
2015-08-21 22:19:04 +0900

    Added more exercises

--- week08/Party.hs
+++ week08/Party.hs
@@ -5,6 +5,7 @@ module Party where
 import Employee
 import Data.Tree
 import Data.Monoid
+import Data.List
 
 -- Exercise 1
 
@@ -17,7 +18,7 @@ glCons e (GL es fun) = GL (e:es) (fun + empFun e)
 
 instance Monoid GuestList where
   mempty  = GL [] 0
-  mappend g1@(GL es1 f1) g2@(GL es2 f2) = GL (es1 ++ es2) $ f1 + f2
+  mappend (GL es1 f1) (GL es2 f2) = GL (es1 ++ es2) $ f1 + f2
 
 -- 3. define moreFun, which takes two GuestLists and returns the one
 -- which has more fun
@@ -29,13 +30,31 @@ moreFun = max
 
 -- implement treeFold for Data.Tree
 
-treeFold :: (a-> b -> b) -> b -> Tree a -> b
-treeFold f z t = foldr f z $ nodeList t
-
-nodeList :: Tree a -> [a]
-nodeList (Node a []) = [a]
-nodeList (Node a sf) = a : (concat . map nodeList) sf
+treeFold :: (a -> [b] -> b) -> Tree a -> b
+treeFold f (Node a []) = f a []
+treeFold f (Node a forest) = f a (map (treeFold f) forest)
 
 -- Exercise 3
 
-nextLevel :: Employee -> [(GuestList, GuestList)] -> [(GuestList, GuestList)]
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel b gls = (withBoss, withoutBoss)
+  where
+    withoutBoss = mconcat $ map (uncurry moreFun) gls
+    withBoss = glCons b $ mconcat $ map snd gls
+
+-- Exercise 4
+
+maxFun :: Tree Employee -> GuestList
+maxFun = uncurry moreFun . treeFold nextLevel
+
+-- Exercise 5
+
+formatGLS :: GuestList -> String
+formatGLS (GL es fun) =
+  totalStr ++ "\n" ++ nameStr
+  where
+    totalStr = "Total fun: " ++ show fun
+    nameStr = unlines $ sort $ map empName es
+
+main :: IO ()
+main = readFile "company.txt" >>= putStrLn . formatGLS . maxFun . read
